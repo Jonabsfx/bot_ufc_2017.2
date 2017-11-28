5d7c77f3aeae27938f246d9ef49fb2b6d013c42f
Jonab Silveira Fernandes <jonabsfx@gmail.com>
2017-11-28 00:45:50 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# fimdosemestrebot
+A really silly bot
+
+# Como usar?
+Aperte [AQUI](http://t.me/fimdosemestrebotufc) para me usar e descobrir quantos dias faltam para as férias da UFC!
---
+++ bot/core.py
@@ -0,0 +1,46 @@
+import re
+import strings
+
+from datetime import datetime, date
+from telegram import InlineQueryResultArticle, InputTextMessageContent, ParseMode
+
+TODAY = date.today()
+END_STRING = '16-12-2017'
+END_DATETIME = datetime.strptime(END_STRING, '%d-%m-%Y')
+END_STRING = END_DATETIME.strftime('%d-%m-%Y')
+
+def result():
+    time_delta = END_DATETIME - datetime.today()
+    results = str(time_delta).split(' ')
+    return results[0]
+
+def start(bot, update):
+    days_left = result()
+    update.message.reply_text(check(days_left))
+
+def inline(bot, update):
+    days_left = result()
+
+    username = update.inline_query.from_user.username
+    if username is None:
+        username = update.inline_query.from_user.first_name
+    phrase = check(days_left)
+
+    results = []
+    results.append(
+        InlineQueryResultArticle(
+            id=1,
+            title=check(days_left),
+            input_message_content=InputTextMessageContent(phrase),
+            parse_mode=ParseMode.MARKDOWN)
+    )
+
+    update.inline_query.answer(results)
+
+def check(days_left):
+    if int(days_left) >= 0:
+        return f'O semestre de {TODAY.year}.2 da UFC acaba em {days_left} dias.'
+    return strings.HORN + f'O semestre de {TODAY.year}.2 da UFC acabou' + strings.PALM_TREE
+
+def get_help(bot, update):
+    update.message.reply_text(strings.HELP_TXT)
